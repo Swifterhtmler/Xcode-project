01081088cc5acbba7ac61c6797b8524f22bfbc79
Riku Kuisma <rikukuisma@icloud.com>
2023-10-15 21:36:16 +0300

    Initial Commit

---
+++ Tracker/TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerApp.swift
+//  Tracker
+//
+//  Created by Riku Kuisma on 15.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
